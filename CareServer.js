84282d7a440246c9f86327e227ca82a39377686f
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-11-13 23:08:22 +0800

    Update CareServer.js

--- CareServer.js
+++ CareServer.js
@@ -62,7 +62,41 @@ if (typeof $response == "undefined") {
       "original_app_user_id": "$RCAnonymousID:f17435b819b0490aaa70cb04025e7938",
       "last_seen": "2024-11-13T13:52:22Z"
     };
-    
+
+    let date = {
+      "expires_date": "2999-01-01T00:00:00Z",
+      "original_purchase_date": "2021-01-01T00:00:00Z",
+      "purchase_date": "2021-01-01T00:00:00Z",
+      "ownership_type": "PURCHASED",
+      "store": "app_store"
+    };
+
+    let subscriber = body.subscriber;
+    let bundle_id = $request.headers["X-Client-Bundle-ID"]
+      ? $request.headers["X-Client-Bundle-ID"]
+      : $request.headers["User-Agent"].match(/^[%a-zA-Z0-9]+/)[0];
+
+    const list = [
+      { "app_name": "Diarly", "bundle_id": "com.pureformstudio.diaryOSX", "product_id": "com.pureformstudio.diary.yearly_2022_promo", "entitlements": ["PRO"], "version": "3.18" },
+      { "app_name": "1Blocker", "bundle_id": "blocker.ios.iap.lifetime", "product_id": "app.pxx917144686", "entitlements": ["premium", "Full_access_app"], "version": "5.8" },
+      { "app_name": "Ereasy", "bundle_id": "background.remover.bg.eraser", "product_id": "app.pxx917144686", "entitlements": ["premium", "Full_access_app"], "version": "2.0.0" }
+    ];
+
+    for (let data of list) {
+      if (bundle_id == data.bundle_id || bundle_id == data.app_name) {
+        let product_id = data.product_id;
+        let entitlements = data.entitlements;
+
+        subscriber.subscriptions[(product_id)] = date;
+
+        for (let entitlement of entitlements) {
+          subscriber.entitlements[(entitlement)] = date;
+          subscriber.entitlements[(entitlement)].product_identifier = product_id;
+        }
+        break;
+      }
+    }
+
     obj.body = JSON.stringify(body);
   }
 }
